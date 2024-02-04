e172df67efe73a8102075891aa068cc81354d172
Denys07748 <denis07748@gmail.com>
2024-02-04 18:07:44 +0200

    refactoring router adds lazy

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,12 @@
+import s from './Layout.module.css';
 import { Suspense } from 'react';
-import Header from './components/Header/Header';
+import Header from 'components/Header/Header';
 import { Outlet } from 'react-router-dom';
 import { Footer } from 'components/Footer/Footer';
 
 const Layout = () => {
   return (
-    <div>
+    <div className={s.container}>
       <Header />
       <main>
         <Suspense>
