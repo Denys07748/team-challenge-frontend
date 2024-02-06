d2a8d011186cb5bfefd9bd53f1c8223330e47e2b
Denys07748 <95140883+Denys07748@users.noreply.github.com>
2024-02-06 21:58:19 +0000

    Deploying to gh-pages from @ Denys07748/team-challenge-frontend@7a24ede71d25e3537b22edfa5f5166fa39c23be3 🚀

--- static/js/807.dd66e895.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkteam_challenge_frontend=self.webpackChunkteam_challenge_frontend||[]).push([[807],{807:function(e,n,t){t.r(n);var a=t(184);n.default=function(){return(0,a.jsx)("div",{children:"Living"})}}}]);
-//# sourceMappingURL=807.dd66e895.chunk.js.map
