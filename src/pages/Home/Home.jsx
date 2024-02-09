0f2e1874ba69cd677b041642d3fc8d3bf21b763b
Chelombitko Vladislav <vladmorilov24@gmail.com>
2024-02-09 17:25:49 +0200

    Adds New Arrivals

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import MainSection from "./MainSection/MainSection";
 import WhyHomewell from "./WhyHomewell/WhyHomewell";
 import ShopBy from "./ShopBy/ShopBy"
+import NewArrivals from "./NewArrivals/NewArrivals";
 
 const Home = () => {
     return <>
         <MainSection />
         <WhyHomewell />
         <ShopBy />
+        <NewArrivals/>
     </>;
 };
 
