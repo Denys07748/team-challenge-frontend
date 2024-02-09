eb3b9a32faae96601666fbd8668cca5250815d16
Denys07748 <denis07748@gmail.com>
2024-02-09 22:30:04 +0200

    fix warning

--- src/pages/Home/NewArrivals/Splide/Splide.Component.jsx
+++ src/pages/Home/NewArrivals/Splide/Splide.Component.jsx
@@ -1,37 +1,44 @@
-import { Splide, SplideSlide , SplideTrack} from '@splidejs/react-splide';
-import { ProductCard } from "../../../../components/Products/ProductCard/ProductCard";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import { ProductCard } from '../../../../components/Products/ProductCard/ProductCard';
 
-import '@splidejs/splide/dist/css/splide.min.css'; 
-import styles from "./Splide.Component.module.css"
+import '@splidejs/splide/dist/css/splide.min.css';
+import styles from './Splide.Component.module.css';
 
 function SplideComponent() {
-      return (
-        <Splide options={{ perPage: 4, focus: 0, omitEnd: true, gap: -110,  pagination: true}}>
-            
-          <SplideSlide className={styles["splide__slide"]}>
-            <ProductCard className={styles.slider}/>
-          </SplideSlide>
-          <SplideSlide className={styles["splide__slide"]}>
-            <ProductCard />
-          </SplideSlide>
-          <SplideSlide className={styles["splide__slide"]}>
-            <ProductCard />
-          </SplideSlide>
-          <SplideSlide className={styles["splide__slide"]}>
-            <ProductCard />
-          </SplideSlide>
-          <SplideSlide className={styles["splide__slide"]}>
-            <ProductCard />
-          </SplideSlide>
-          <SplideSlide className={styles["splide__slide"]}>
-            <ProductCard />
-          </SplideSlide>
-          <SplideSlide className={styles["splide__slide"]}>
-            <ProductCard />
-          </SplideSlide>
-          <div className={styles["splide__pagination"]}></div>
-        </Splide>
-      );
-    };
+  return (
+    <Splide
+      options={{
+        perPage: 4,
+        focus: 0,
+        omitEnd: true,
+        gap: -110,
+        pagination: true,
+      }}
+    >
+      <SplideSlide className={styles['splide__slide']}>
+        <ProductCard className={styles.slider} />
+      </SplideSlide>
+      <SplideSlide className={styles['splide__slide']}>
+        <ProductCard />
+      </SplideSlide>
+      <SplideSlide className={styles['splide__slide']}>
+        <ProductCard />
+      </SplideSlide>
+      <SplideSlide className={styles['splide__slide']}>
+        <ProductCard />
+      </SplideSlide>
+      <SplideSlide className={styles['splide__slide']}>
+        <ProductCard />
+      </SplideSlide>
+      <SplideSlide className={styles['splide__slide']}>
+        <ProductCard />
+      </SplideSlide>
+      <SplideSlide className={styles['splide__slide']}>
+        <ProductCard />
+      </SplideSlide>
+      <div className={styles['splide__pagination']}></div>
+    </Splide>
+  );
+}
 
-export default SplideComponent
+export default SplideComponent;
