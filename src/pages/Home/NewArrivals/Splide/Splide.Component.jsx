4e1e765d56d58c89905323c112fdfb5beb787978
Chelombitko Vladislav <vladmorilov24@gmail.com>
2024-02-19 18:36:25 +0200

    StylesSplide fix

--- src/pages/Home/NewArrivals/Splide/Splide.Component.jsx
+++ src/pages/Home/NewArrivals/Splide/Splide.Component.jsx
@@ -1,4 +1,5 @@
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import SwiperCore from "swiper"
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import { Splide, SplideSlide } from '@splidejs/react-splide';
 import { ProductCard } from '../../../../components/Products/ProductCard/ProductCard';
@@ -8,9 +9,9 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-import "./Splide.Component.module.css"
+import "./Splide.css"
 
-
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y]);
 function SplideComponent() {
   return (
     <Swiper
@@ -21,6 +22,11 @@ function SplideComponent() {
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
     >
+      <SwiperSlide><ProductCard /></SwiperSlide>
+      <SwiperSlide><ProductCard /></SwiperSlide>
+      <SwiperSlide><ProductCard /></SwiperSlide>
+      <SwiperSlide><ProductCard /></SwiperSlide>
+      <SwiperSlide><ProductCard /></SwiperSlide>
       <SwiperSlide><ProductCard /></SwiperSlide>
       <SwiperSlide><ProductCard /></SwiperSlide>
       <SwiperSlide><ProductCard /></SwiperSlide>
