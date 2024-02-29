15597b2b0fe9fecd1f7261b6c3ed9ccdc637f571
Vlad1slov <vladmorilov24@gmail.com>
2024-02-29 19:36:25 +0200

    CustomerRevies Fix

--- src/pages/Home/CustomerRevies/CustomerRevies.jsx
+++ src/pages/Home/CustomerRevies/CustomerRevies.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./CustomerRevies.module.css"
+import img1 from "../../../images/rectangle-23.jpg"
+import img2 from "../../../images/Rectangle-21.jpg"
+import img3 from "../../../images/Rectangle-4.jpg"
 
  function CustomerRevies() {
-//   const dataComments = {
-//     person:{
-//         comment: "I love Homewell  vnvjnfqyu hefubfbevin",
-//         name: "Vlad C",
-//         photo: "../images/rectangle-21.jpg"
-//     },
-//     person2: {
-//         comment: "I love Homewell2 jfvnfjvbpqcmvfhvnbwydwmfef",
-//         name: "Vlad C2",
-//         photo: "../images/rectangle-21.jpg"
-//     },
-//     person3: {
-//         comment: "I love Homewell3 ahhdvidnvkdanvkadnvian jnfjvnjnvn nkvnkv",
-//         name: "Vlad C3",
-//         photo: "../images/rectangle-21.jpg"
-//     }
-// };
+
+  const [activeUser, setActiveUser] = useState(1)
+
+  const dataComments = [
+    {
+      id:1,
+        comment: "Homewell is a haven for style enthusiasts like myself. The curated collection is a testament to their dedication to aesthetics and quality. Each piece feels like a work of art, and I'm thrilled with the transformative impact they've had on my living space."
+        ,
+        name: "Vlad C",
+        photo: img1
+    },
+    {
+        id: 2,
+        comment: "I love Homewell2 jfvnfjvbpqcmvfhvnbwydwmfef",
+        name: "Vlad C2",
+        photo: img2
+    },
+    {  
+        id: 3,
+        comment: "I love Homewell3 ahhdvidnvkdanvkadnvian jnfjvnjnvn nkvnkv",
+        name: "Vlad C3",
+        photo: img3
+    }
+];
+
+const handlePhotoClick = (photoId) => {
+  setActiveUser(photoId);
+};
+
   return (
     <div className={styles.mainContainer}>
       <div className={styles.frame}>
@@ -28,34 +43,32 @@ import styles from "./CustomerRevies.module.css"
       </div>
       <div className={styles.frame1}>
         <span className={styles.homewellTestimonial}>
-          "Homewell is a haven for style enthusiasts like myself. The curated
-          collection is a testament to their dedication to aesthetics and
-          quality. Each piece feels like a work of art, and I'm thrilled with
-          the transformative impact they've had on my living space."
+          {dataComments.find((person) => person.id === activeUser).comment}
         </span>
       </div>
       <div className={styles.frame2}>
         <div className={styles.frame3}>
-          <div className={styles.ellipse} />
-          <div className={styles.frame4}>
-            <div className={styles.frame5}>
-              <span className={styles.name}>Alex R.</span>
+          {dataComments.map((person) => (
+            <div key={person.id} className={styles.personContainer}>
+            <img 
+              key={person.id} 
+              src={person.photo} 
+              alt={`Photo ${person.id}`} 
+              className={`${activeUser === person.id ? styles.ellipse : styles.ellipseNone}`} 
+              onClick={() => handlePhotoClick(person.id)}/>
+
+              <div className={styles.frame5}>
+              <span className=
+              {`${activeUser === person.id ? styles.name : styles.nameNone}`}
+              >{dataComments.find((person) => person.id === activeUser).name}</span>
             </div>
+            </div>
+          ))}
+          <div className={styles.frame4}>
+            
           </div>
         </div>
-        {/* {dataComments.map((el, i) =>  
-        <div key={i}>
-            <div className={styles.ellipseNone}>
-              {el.comment}
-            </div>
-            <div>
-              {el.photo}
-            </div>
-            <div>
-              {el.name}
-            </div>
-        </div>
-        )} */}
+        
       </div>
     </div>
   );
