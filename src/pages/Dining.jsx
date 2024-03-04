2c11bb0cf56387d22384875f9252d0bf7519b98a
Denys07748 <denis07748@gmail.com>
2024-03-04 22:42:39 +0200

    adds DiningHero

--- src/pages/Dining.jsx
+++ src/pages/Dining.jsx
@@ -1,5 +1,12 @@
+import { Goods } from 'components/Products/Goods';
+import diningImg from 'images/rectangle-19.jpg';
+
 const Dining = () => {
-    return <div>Dining</div>;
+  return (
+    <div>
+      <Goods title="Dining" img={diningImg} />
+    </div>
+  );
 };
 
 export default Dining;
